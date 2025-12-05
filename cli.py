dcb6a66a4b1fcf067bb1bee58a4bbc3d263db288
Kristian Kanchev <kristianfzr@gmail.com>
2025-12-05 09:57:23 +0000

    Add summary for all expenses

--- cli.py
+++ cli.py
@@ -11,13 +11,14 @@ filename = expense_DIR / "expenses.csv"
 fields = ['ID', 'Date', 'Description', 'Amount']
 
 parser = argparse.ArgumentParser(description='Espense Tracker')
-subparsers = parser.add_subparsers(dest='command', help='Available commands')
+subparsers = parser.add_subparsers(dest='command', required=True, help='Available commands')
 
 add_parser = subparsers.add_parser('add', help='Add new expense')
 add_parser.add_argument('--description', type=str, required=True, help='Description of the expense')
 add_parser.add_argument('--amount', type=float, required=True, help='Amount of the expense')
 
 list_parser = subparsers.add_parser('list', help='List all expenses')
+list_parser = subparsers.add_parser('summary', help='Summarize all expenses')
 
 args = parser.parse_args()
 
@@ -63,8 +64,26 @@ def list_expenses(filename):
         tab.add_row([expense['ID'], expense['Date'], expense['Description'], expense['Amount']])
     print(tab)
     
+def summary(filename):
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            try:
+                reader = csv.DictReader(file)
+                expenses = list(reader)
+            except csv.Error:
+                expenses = []
+    else:
+        expenses = []
+    
+    sum_expenses = []
+    for expense in expenses:
+        sum_expenses.append(float(expense['Amount']))
+    print(f"${sum(sum_expenses)}")
+    
 if __name__ == "__main__":
     if args.command == 'add':
         add_expense(filename, args.description, args.amount)
     elif args.command == 'list':
         list_expenses(filename)
+    elif args.command == 'summary':
+        summary(filename)
